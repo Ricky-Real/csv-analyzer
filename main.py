350a2abab387f53787427248dd9d51486380ca1b
Ricky Real <327527124+Ricky-Real@users.noreply.github.com>
2026-09-10 16:34:51 +0200

    Added age statistics

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import csv
 
 with open("example.csv", "r") as f:
-    data = list(csv.reader(f))
+    data = list(csv.DictReader(f))
+
+ages = [int(person["age"]) for person in data]
 
 print(f"Number of records {len(data)}")
+print(f"Average age: {sum(ages) / len(ages):.1f}")
+print(f"Youngest: {min(ages)}")
+print(f"Oldest: {max(ages)}")
