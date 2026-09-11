ea0919fd04699260bce2896ff20d3d2171def7c0
Ricky Real <327527124+Ricky-Real@users.noreply.github.com>
2026-09-11 21:20:37 +0200

    Added new features

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 import csv
 
-with open("example.csv", "r") as f:
-    data = list(csv.DictReader(f))
+class Reader():
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.data = self.load_data()
+        self.records = len(self.data)
 
-ages = [int(person["age"]) for person in data]
+    def load_data(self):
+        with open(self.file_path, "r") as f:
+            data = list(csv.DictReader(f))
+        return data
+    
+    def get_column(self, key: str | None = None):
+        if key is None:
+            return "Please provide a key"
+        try:
+            line = line[key]
+        except KeyError:
+            return "Please provide a VALID key"
+        
+        new_list = []
+        for line in self.data:
+            try:
+                line = int(line[key])
+                new_list.append(line)
+            except ValueError:
+                try:
+                    line = float(line[key])
+                    new_list.append(line)
+                except ValueError:
+                    new_list.append(line[key])
+        return new_list
 
-print(f"Number of records {len(data)}")
-print(f"Average age: {sum(ages) / len(ages):.1f}")
-print(f"Youngest: {min(ages)}")
-print(f"Oldest: {max(ages)}")
+    def max_item(self, list: list):
+        return max(list)
+
+    def min_item(self, list: list):
+        return min(list)
+
+    def average_val(self, list: list, n_digits: int | None = None):
+        return round(sum(list)/len(list), n_digits)
+    
+if __name__ == "__main__":
+    reader = Reader("example.csv")
+    names = reader.get_column("name")
+    ages = reader.get_column("age")
+    print(names)
+    print(ages)
+    print(reader.max_item(names))        
+    print(reader.min_item(names))
+    print(reader.average_val(ages))
